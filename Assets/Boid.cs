235c756f1e4a25990bda870112398755a4ef8232
Simon O'Neill <supercoolsimon@hotmail.com>
2018-03-13 11:00:28 +0000

    Submission

---
+++ Assets/Boid.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    public float Mass;
+    public float MaxSpeed;
+
+    public bool IsSeeking;
+    public bool IsOffsetPursue;
+    public bool IsCameraFocusedOn;
+
+    private Vector3 Force;
+    private Vector3 Acceleration;
+    private Vector3 Velocity;
+
+    public GameObject Target;
+    public Vector3 OffSet;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Force = Calculate();
+        //rb.AddForce(Force);
+
+        Acceleration = Force / Mass;
+
+        var newVelocity = Acceleration * Time.deltaTime;
+
+        Velocity = Vector3.Lerp(Velocity, newVelocity, 0.15f);
+        Velocity = Vector3.ClampMagnitude(Velocity, MaxSpeed);
+
+        //if (IsSeeking) { Debug.Log(Velocity.magnitude); }
+
+        transform.position += (Velocity * Time.deltaTime);
+
+        if(IsCameraFocusedOn)
+        {
+            var camera = FindObjectOfType<Camera>();
+            camera.transform.position = transform.position + new Vector3(0,20,0);
+            camera.transform.rotation = Quaternion.EulerAngles(90, 0, 0);
+        }
+	}
+
+    Vector3 Calculate()
+    {
+        Vector3 total = Vector3.zero;
+
+        if(IsSeeking)
+        {
+            total += Seek(Target.transform.position);
+        }
+        if (IsOffsetPursue)
+        {
+            total += OffsetPursue(OffSet,Target.transform.position);
+        }
+
+        return total;
+    }
+
+    Vector3 Seek(Vector3 targetPos)
+    {
+        var direction = (targetPos- transform.position).normalized;
+        var desiredVelocity = direction * MaxSpeed;
+
+        return desiredVelocity;
+    }
+
+    Vector3 OffsetPursue(Vector3 offSet,Vector3 target)
+    {
+        return Seek(target + offSet);
+    }
+}
---
+++ Assets/EagleSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour {
+
+    public float gap;
+    public float followers;
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void Awake()
+    {
+        var eagle = Instantiate(prefab);
+        eagle.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        var leaderBoid = eagle.GetComponent<Boid>();
+        var target = new GameObject();
+        target.transform.Translate(new Vector3(transform.position.x, transform.position.y, transform.position.z + 1000));
+
+        leaderBoid.Target = target;
+        leaderBoid.IsSeeking = true;
+        leaderBoid.IsCameraFocusedOn = true;
+
+        for (int i=1;i<followers;i++)
+        {
+            var offSet1 = new Vector3(gap * i, 0, gap*i);
+            var offSet2 = new Vector3(gap * i, 0, -gap*i);
+
+            CreateEagleWithOffset(offSet1,eagle);
+            CreateEagleWithOffset(offSet2,eagle);
+        }
+    }
+
+    private void CreateEagleWithOffset(Vector3 offset,GameObject target)
+    {
+        var newEagle = Instantiate(prefab);
+        newEagle.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        newEagle.transform.Translate(offset);
+        var newBoid1 = newEagle.GetComponent<Boid>();
+        newBoid1.Target = target;
+        newBoid1.OffSet = new Vector3(offset.z, offset.y, -offset.x);
+        newBoid1.IsOffsetPursue = true;
+    }
+}
